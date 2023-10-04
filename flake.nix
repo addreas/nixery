3571eeb524c7d926a0f5b76dd01d7665978fa8f2
Andreas Mårtensson <andreas@addem.se>
2023-10-04 22:11:39 +0200

    flake

--- flake.nix
+++ flake.nix
@@ -10,93 +10,81 @@
     eachSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        nixery = pkgs.buildGoModule rec {
+          name = "nixery";
+          src = pkgs.lib.sources.sourceFilesBySuffices ./. [ ".go" "prepare-image.nix" "go.mod" "go.sum" ];
+          doCheck = false;
+
+          subPackages = [ "cmd/server" ];
+
+          # Needs to be updated after every modification of go.mod/go.sum
+          vendorHash = "sha256-RK6uG7UzJo4UJ5uibtq0bXvjalJUWMYUU91FfxOxDF0=";
+
+          ldflags = [
+            "-s"
+            "-w"
+            "-X"
+            "main.version=${self.sourceInfo.narHash}"
+            "-X"
+            "builder.hostSystem=${system}"
+          ];
+
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          postInstall = ''
+            wrapProgram $out/bin/server \
+              --set WEB_DIR "${./web}" \
+          '';
+        };
       in
       {
         formatter.${system} = pkgs.nixpkgs-fmt;
 
         packages.${system} = {
-          default = self.packages.${system}.nixery;
-          nixery = pkgs.buildGoModule rec {
-            name = "nixery";
-            src = pkgs.lib.sources.sourceFilesBySuffices ./. [".go" "prepare-image.nix" "go.mod" "go.sum"];
-            doCheck = true;
-
-            subPackages = [ "cmd/server" ];
-
-            # Needs to be updated after every modification of go.mod/go.sum
-            vendorHash = "sha256-RK6uG7UzJo4UJ5uibtq0bXvjalJUWMYUU91FfxOxDF0=";
-
-            ldflags = [
-              "-s"
-              "-w"
-              "-X"
-              "main.version=${self.sourceInfo.narHash}"
-              "-X"
-              "builder.hostSystem=${system}"
-            ];
-
-            nativeBuildInputs = [ pkgs.makeWrapper ];
-            postInstall = ''
-              wrapProgram $out/bin/server \
-                --set WEB_DIR "${./web}" \
-            '';
-          };
+          default = nixery;
+          nixery = nixery;
         };
 
         # Container image containing Nixery and Nix itself. This image can
         # be run on Kubernetes, published on AppEngine or whatever else is
         # desired.
         images.${system}.nixery =
-          let
-            # Wrapper script for the wrapper script (meta!) which configures
-            # the container environment appropriately.
-            #
-            # Most importantly, sandboxing is disabled to avoid privilege
-            # issues in containers.
-            nixery-launch-script = pkgs.writeShellScriptBin "nixery" ''
-              set -e
-              export PATH=${pkgs.coreutils}/bin:$PATH
-              export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt
-              mkdir -p /tmp
-
-              # Create the build user/group required by Nix
-              echo 'nixbld:x:30000:nixbld' >> /etc/group
-              echo 'nixbld:x:30000:30000:nixbld:/tmp:/bin/bash' >> /etc/passwd
-              echo 'root:x:0:0:root:/root:/bin/bash' >> /etc/passwd
-              echo 'root:x:0:' >> /etc/group
-
-              # Disable sandboxing to avoid running into privilege issues
-              mkdir -p /etc/nix
-              echo 'sandbox = false' >> /etc/nix/nix.conf
-
-              # In some cases users building their own image might want to
-              # customise something on the inside (e.g. set up an environment
-              # for keys or whatever).
-              #
-              # This can be achieved by setting a '/etc/nixery/pre-launch.sh' script.
-              [[ -x /etc/nixery/pre-launch.sh ]] && /etc/nixery/pre-launch.sh
-
-              exec ${self.packages.${system}.nixery}/bin/server
-            '';
-          in
           pkgs.dockerTools.buildLayeredImage {
             name = "nixery";
-            config.Cmd = [ "${nixery-launch-script}/bin/nixery" ];
+            config.Cmd = [ "server" ];
+            config.User = "1000:1000";
+            config.Env = [
+              "NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt"
+              # "NIX_STORE_DIR=/nixery/nix/store"
+              # "NIX_LOG_DIR=/nixery/nix/var/log"
+              # "NIX_STATE_DIR=/nixery/nix/var"
+            ];
+
+            extraCommands = ''
+              mkdir -p ./tmp ./nix/{store,var/nix}
+              chmod -R a+w ./tmp ./nix/{store,var/nix}
+
+              mkdir -p ./etc/nix
+              echo 'sandbox = false' >> ./etc/nix/nix.conf
+              echo 'experimental-features = nix-command flakes' >> ./etc/nix/nix.conf
+            '';
 
             maxLayers = 20;
             contents = with pkgs; [
-              dockerTools.fakeNss
+              (dockerTools.fakeNss.override {
+                extraPasswdLines = [ "nixery:x:1000:1000:nixery:/nixery:/bin/sh" ];
+                extraGroupLines = [ "nixery:x:1000:" ];
+              })
               bashInteractive
               cacert
               coreutils
-              git
-              gnutar
-              gzip
-              iana-etc
+              # git
+              # gnutar
+              # gzip
+              # iana-etc
               nix
-              nixery-launch-script
-              openssh
-              zlib
+              nixery
+              # openssh
+              # zlib
             ];
           };
       });
